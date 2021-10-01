1eec26449ed3949dab06b6885176ab9a37be9cd3
unknown <kuwar51@yahoo.com>
2021-10-01 23:43:43 +0530

    msg

--- js/index.js
+++ js/index.js
@@ -294,7 +294,7 @@ function reveal() {
   }
 
   var ifrm = document.createElement("iframe");
-  ifrm.setAttribute("src", "/video/hbd.mp4");
+  ifrm.setAttribute("src", "https://www.youtube.com/embed/r1Oavh8dPkc?controls=0&loop=1&autoplay=1");
   //ifrm.style.width = `${w}px`;
   //ifrm.style.height = `${h}px`;
   ifrm.style.border = 'none';
